f6bdd9947c59d52939786daead4e31c62916a31a
Thiago Vieira <thiagovieiraas@gmail.com>
2014-10-14 17:38:44 -0500

    Added getters and setters

--- Board.java
+++ Board.java
@@ -13,6 +13,31 @@ public class Board {
 				this._board[i][j] = -1;
 	}
 	
+	public void setValue(int row, int column, int value) throws Exception
+	{
+		if(!isValueValid(value)) throw new Exception("Invalid value.");
+		if(!isPositionValid(row, column)) throw new Exception("Invalid position.");
+		this._board[row][column] = value;
+	}
+	
+	public int getValue(int row, int column) throws Exception
+	{
+		if(!isPositionValid(row, column)) throw new Exception("Invalid position.");
+		return this._board[row][column];
+	}
+	
+	// Check if the position informed is valid.
+	public boolean isPositionValid(int row, int column)
+	{
+		return !(row < 0 || column < 0 || row > 8 || column > 8); 
+	}
+	
+	// Check if the value informed is valid.
+	public boolean isValueValid(int value)
+	{
+		return (value<10 && value>0);
+	}
+	
 	private void resetAux(int[] aux)
 	{
 		aux = new int[9];
@@ -42,7 +67,7 @@ public class Board {
 			for(int j = 0; j < 9; j++)
 			{
 				if(this._board[i][j] == -1) return false;
-				if(this._board[i][j] > 9) return false;
+				if(isValueValid(this._board[i][j])) return false;
 				line[this._board[i][j]]=1;
 				column[this._board[j][i]]=1;
 			}
