8771c0f8a99f2d133533d886dd403c3f6f46b722
Thiago Vieira <thiagovieiraas@gmail.com>
2014-10-14 16:30:19 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,25 +1,36 @@
 import java.util.*;
 
 public class Board {
-	private String[] _board;
-	private final String initialLine = "*********";
+	private int[][] _board;
 	
 	public Board()
 	{ reset(); }
 	
 	public void reset()
 	{
-		this._board = new String[9];
+		this._board = new int[9][9];
 		for(int i = 0; i < 9; i++)
-			this._board[i] = this.initialLine;
+			for(int j = 0; j < 9; j++)
+				this._board[i][j] = -1;
+	}
+	
+	private void resetAux(int[] aux)
+	{
+		aux = new int[9];
+		for(int i = 0; i < 9; i++) aux[i]=0;
 	}
 	
 	public boolean isSolved()
 	{
+		int[] line = null, column = null;
+		resetAux(line);
+		resetAux(column);
+		
 		for(int i = 0; i < 9; i++)
 		{
 			for(int j = 0; j < 9; j++)
 			{
+				if(this._board[i][j] == -1) return false;
 				
 			}
 		}
